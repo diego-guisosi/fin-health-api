59e685facc1f049535608b8df76fd00824de65e9
Diego <diego.guisosi@gmail.com>
2019-01-19 23:15:02 -0200

    create spring boot web mvn app

---
+++ src/main/java/br/com/dixy/finhealth/FinHealthApplication.java
@@ -0,0 +1,14 @@
+package br.com.dixy.finhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinHealthApplication.class, args);
+	}
+
+}
+
